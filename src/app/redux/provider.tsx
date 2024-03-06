87b279d1866c446034a10a5b6b6157080ed92649
Frederick Go <dn282145@dal.ca>
2024-03-06 12:58:37 -0400

    Build error fixed

--- src/app/redux/provider.tsx
+++ src/app/redux/provider.tsx
@@ -2,7 +2,11 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store";
-
-export function Providers({ children }) {
+import { DarkModeState } from "./Features/darkModeSlice";
+import { ReactNode } from "react";
+interface ProvidersProps {
+  children: ReactNode;
+}
+export function Providers({ children }: ProvidersProps) {
   return <Provider store={store}>{children}</Provider>;
 }
