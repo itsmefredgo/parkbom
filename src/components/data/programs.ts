51c4bdff8fe0fa6617173efc778a841949f39c1b
Frederick Go <dn282145@dal.ca>
2024-03-08 13:34:37 -0400

    Programs section, minor visual issues fixed

--- src/components/data/programs.ts
+++ src/components/data/programs.ts
@@ -1,39 +1,39 @@
-export interface MethodItem {
-  title: string;
+export interface ProgramItem {
+  programAttribute: string;
   description: string;
 }
 
-const methodItems: MethodItem[] = [
+const programItems: ProgramItem[] = [
   {
-    title: "소수정예반",
+    programAttribute: "소수정예반",
     description:
       "소수정예반은 일반 수업보다 높은 난이도와 심화된 내용을 제공하는 수업입니다. 높은 학습 능력과 흥미를 가진 학생들을 대상으로 하며, 일반적으로 더 고급 수학 및 과학 주제에 초점을 맞출 수 있습니다.",
   },
   {
-    title: "1:1 개별지도",
+    programAttribute: "1:1 개별지도",
     description:
       "개별지도는 개별 학생의 학습 수준과 필요에 맞게 맞춤형 지도를 제공하는 수업 형태입니다. 학생 개개인의 강점과 약점을 고려하여 개인별 맞춤 교육을 제공하며, 학습 속도와 스타일에 맞춰 유연하게 조절됩니다.",
   },
   {
-    title: "학교별 맞춤 전략",
+    programAttribute: "학교별 맞춤 전략",
     description:
       "학교별 맞춤 전략은 특정 학교의 교육 체계와 요구에 맞게 수업을 설계하는 것을 의미합니다. 학교가 특정 교육 방식을 강조하거나 특별한 교육 목표를 가지고 있을 때 이를 반영하여 학생들에게 맞춤형 교육을 제공합니다.",
   },
   {
-    title: "실전 모의고사",
+    programAttribute: "실전 모의고사",
     description:
       "실전 모의고사는 학생들에게 실제 시험과 유사한 환경에서 모의고사를 치르게 하는 수업입니다. 학생들이 시험에 대비하면서 실제 시험과 유사한 상황에서 자신의 실력을 확인하고 향상시킬 수 있도록 도와줍니다.",
   },
   {
-    title: "기초/개념 이해",
+    programAttribute: "기초/개념 이해",
     description:
       "기초/개념 이해는 학문의 기초적인 원리와 개념을 학생들에게 이해시키는 데 중점을 둔 수업입니다. 기본 개념을 탄탄하게 이해하고 익힘으로써 학문 전반에 걸쳐 깊은 이해를 기반으로 하는 학습을 가능케 합니다.",
   },
   {
-    title: "자기주도 학습력",
+    programAttribute: "자기주도 학습력",
     description:
       "자기주도 학습력은 학생이 스스로 학습 목표를 설정하고 학습 계획을 세우며, 주도적으로 학습하는 능력을 의미합니다. 이는 학생들이 미래에도 계속해서 새로운 지식을 습득하고 스스로 문제를 해결할 수 있는 역량을 갖추는 데 도움이 됩니다.",
   },
 ];
 
-export default methodItems;
+export default programItems;
