d24bbdfd83e229661bc7e6f6d46d5a0330ac3bb5
Frederick Go <dn282145@dal.ca>
2024-03-12 10:46:02 -0300

    Mobile Header implemented

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -7,9 +7,9 @@ export default function AboutUs() {
   return (
     <div
       className=" bg-[#efefec]
-      px-[5vw] laptop:bg-blue-500"
+      px-[5vw] laptop:bg-blue-500 pt-[6rem] "
     >
-      <div className="pt-[6rem] text-[3rem] mb-8 text-[#045329] font-bold">
+      <div className="text-[3rem] mb-8 text-[#045329] font-bold">
         <h1>ABOUT US</h1>
       </div>
 
--- src/components/includes/Header.tsx
+++ src/components/includes/Header.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import ToggleDarkMode from "../helpers/ToggleDarkMode";
+import Image from "next/image";
+import parkbom_logo from "@/assets/images/parkbom-logo-tpbg.png";
+import { FaBars } from "react-icons/fa6";
+import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
 export default function Header() {
   const pages: [string, string][] = [
@@ -8,25 +15,81 @@ export default function Header() {
     ["문의 및 위치", "/inquiries"],
   ];
 
+  const [initialRun, setInitialRun] = useState(false);
+  const [navFolded, setNavFolded] = useState(true);
+
+  function extendNavBar() {
+    setNavFolded(!navFolded);
+    if (!initialRun) setInitialRun(true);
+  }
+
   return (
     <header
-      className=" flex justify-around items-center shadow-lg text-xl fixed w-screen h-20 z-10 font-semibold 
+      className=" shadow-lg text-xl fixed top-0 w-screen h-20 z-10 font-semibold 
       text-parkbomDarkText dark:text-parkbomLightText
       bg-parkbomLightBg02 dark:bg-parkbomDarkBg02
         bg-opacity-60 dark:bg-opacity-60"
     >
-      <div className=" flex">
-        <Link href={"/"}>박봄 수학 영어</Link>
-        <div className="ml-12">|</div>
+      <div className=" flex flex-row justify-between h-full items-center pl-8 pr-12">
+        <Link href={"/"} className=" ">
+          <Image
+            src={parkbom_logo}
+            alt="Parkbom"
+            className=" dark:brightness-200 h-[4rem] w-[auto]"
+          ></Image>
+        </Link>
+        <button onClick={extendNavBar}>
+          <FaBars />
+        </button>
+        {/* <div className="ml-12 hidden">|</div> */}
+        {/* {pages.map(([pageName, route], index) => (
+          <Link key={index} href={route} className=" ml-12 hidden">
+            {pageName}
+          </Link>
+        ))} */}
+      </div>
+
+      <div
+        className={` translate-x-[100%] fixed w-screen h-screen top-0
+        bg-[#c1d0c1] z-50 flex flex-col pt-16 pr-16 ${
+          initialRun &&
+          (navFolded
+            ? " animate-[foldHeaderAnimation_1s_ease_forwards]"
+            : " animate-[extendHeaderAnimation_1s_ease_forwards]")
+        }`}
+      >
+        {/* className=" fixed w-screen h-screen top-0  bg-red-500 z-50 hidden" */}
+        <button
+          className="mr-[-0.5rem] ml-auto h-[2rem] w-[2rem]"
+          onClick={extendNavBar}
+        >
+          <FaTimes className="h-full w-full" />
+        </button>
+        <Link
+          onClick={extendNavBar}
+          href={"/"}
+          className=" ml-auto mt-4 mr-[-0.5rem]"
+        >
+          <Image
+            src={parkbom_logo}
+            alt="Parkbom"
+            className=" dark:brightness-200 h-[3rem] w-[auto]"
+          ></Image>
+        </Link>
         {pages.map(([pageName, route], index) => (
-          <Link key={index} href={route} className=" ml-12">
+          <Link
+            key={index}
+            href={route}
+            onClick={extendNavBar}
+            className=" mr-0 ml-auto mt-4"
+          >
             {pageName}
           </Link>
         ))}
       </div>
-      <div className="">
+      {/* <div className=" hidden">
         <ToggleDarkMode></ToggleDarkMode>
-      </div>
+      </div> */}
     </header>
   );
 }
