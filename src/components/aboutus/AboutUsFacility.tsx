4b944f22f7dc90722f630a84146b34a48e4bb4e2
Frederick Go <dn282145@dal.ca>
2024-03-21 20:02:07 -0300

    update: about us page > gallaries changed to facilities, layout updated

--- src/components/aboutus/AboutUsFacility.tsx
+++ src/components/aboutus/AboutUsFacility.tsx
@@ -1,10 +1,23 @@
 // Importing necessary react icons
 import { CiImageOn } from "react-icons/ci";
+import PlaceHolder from "@/assets/images/temp.jpg";
+import Image from "next/image";
 
 function AboutUsFacility() {
+  const facilitiesGridClasses = `[&>*:nth-child(2)]:sm:col-start-2 
+  [&>*:nth-child(2)]:sm:col-end-4 [&>*:nth-child(2)]:sm:row-start-1 
+  [&>*:nth-child(2)]:sm:row-end-2 [&>*:nth-child(5)]:sm:col-start-2 
+  [&>*:nth-child(5)]:sm:col-end-4 [&>*:nth-child(5)]:sm:row-start-2 
+  [&>*:nth-child(5)]:sm:row-end-4 [&>*:nth-child(7)]:sm:col-start-2 
+  [&>*:nth-child(7)]:sm:col-end-4 [&>*:nth-child(7)]:sm:row-start-4 
+  [&>*:nth-child(7)]:sm:row-end-5`;
+
   // Fourth part of parkbom's ABOUT US page
   return (
-    <div className="px-[12vw] pb-[10rem] pt-[10rem] laptop:max-w-[80rem] laptop:mx-auto">
+    <div
+      className=" px-[1rem] sm:px-[5rem] md:w-[90%] lg:w-[60rem] xl:w-[65rem] 
+      my-[10rem] mx-[1rem] sm:mx-auto"
+    >
       {/* Heading of the section */}
       <h2
         className=" text-[2.5rem] font-bold pb-16 pl-2 border-black 
@@ -13,43 +26,72 @@ function AboutUsFacility() {
         시설
       </h2>
       <div
-        // className=" sm:grid sm:grid-cols-2 laptop:grid-cols-3 gap-4 "
-        className=" grid  "
+        className={` grid grid-cols-1 grid-rows-[repeat(7,1fr)] ${facilitiesGridClasses}
+        sm:grid-cols-3 sm:grid-rows-[repeat(4,1fr)] gap-2 w-full h-auto `}
       >
-        <div>
-          <CiImageOn></CiImageOn>
-          <CiImageOn></CiImageOn>
-          <CiImageOn></CiImageOn>
-          <CiImageOn></CiImageOn>
+        <div className=" w-full relative h-[80vw] sm:h-[20vw] sm:max-h-[18rem]">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        {/* <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
-        <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
-        <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
-        <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn> */}
-        {/* <div className=" mt-4">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        <div className=" mt-4">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        <div className=" mt-4 sm:mt-8 laptop:mt-4">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        <div className=" mt-4 sm:mt-8">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        <div className=" mt-4 sm:mt-8">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
         </div>
-        <div className=" mt-4 sm:mt-8">
-          <div className="ml-2 mb-2"></div>
-          <CiImageOn className="w-[100%] h-[auto] border-2 border-parkbomDarkBg02"></CiImageOn>
-        </div> */}
+        <div className=" w-full h-full relative">
+          <Image
+            src={PlaceHolder}
+            alt="Parkbom"
+            objectFit="cover"
+            layout="fill"
+            className=" "
+          ></Image>
+        </div>
       </div>
     </div>
   );
